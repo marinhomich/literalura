1e9827c743e4675b16795ec09efab0220385a9de
Michel Marinho <marinhomich@gmail.com>
2024-06-26 15:58:41 -0400

    Initial Commit

---
+++ src/main/java/dev/marinhomich/literaula/LiteraulaApplication.java
@@ -0,0 +1,13 @@
+package dev.marinhomich.literaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marinhomich/literaula/LiteraulaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marinhomich.literaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
